d1fdb5f527c224559a61bba7fa22b0f912dc61eb
agiammarchi <agiammarchi@twitter.com>
2013-02-19 15:03:38 -0800

    even smarter .test.js file, now updating web part too

--- test/.test.js
+++ test/.test.js
@@ -3,6 +3,8 @@ var
   path = require('path'),
   spawn = require('child_process').spawn,
   modules = path.join(__dirname, '..', 'node_modules', 'wru', 'node', 'program.js'),
+  tests = [],
+  ext = /\.js$/,
   code = 0,
   many = 0;
 
@@ -11,13 +13,24 @@ function exit($code) {
     code = $code;
   }
   if (!--many) {
+    if (!code) {
+      fs.writeFileSync(
+        path.join(__dirname, '..', 'index.html'),
+        fs.readFileSync(
+          path.join(__dirname, '..', 'index.html'),
+          'utf-8'
+        ).replace(/var TESTS = \[.*?\];/, 'var TESTS = ' + JSON.stringify(tests) + ';'),
+        'utf-8'
+      );
+    }
     process.exit(code);
   }
 }
 
 fs.readdirSync(__dirname).filter(function(file){
-  if (fs.existsSync(path.join(__dirname, '..', 'src', file))) {
+  if (ext.test(file) && fs.existsSync(path.join(__dirname, '..', 'src', file))) {
     ++many;
+    tests.push(file.replace(ext, ''));
     spawn(
       'node', [modules, path.join('test', file)], {
       detached: false,
